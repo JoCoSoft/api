e36c9c081488b21b7c5be8582ffba332e61e3247
Eddie Hedges <eddiehedges@gmail.com>
2018-08-06 15:56:00 -0500

    Added some docs and a reset dev db script

---
+++ README.md
@@ -0,0 +1,5 @@
+### Sequelize
+
+Use `node_modules/.bin/sequelize` to see the available commands.
+
+Resetting the development database can be done by the `bin/reset-devdb` script.
--- migrations/20180804074947-initialSchema.js
+++ migrations/20180804074947-initialSchema.js
@@ -6,23 +6,9 @@ module.exports = {
       name: Sequelize.STRING,
       data: Sequelize.JSON
     });
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+  down: queryInterface => {
     return queryInterface.dropTable("jobs");
   }
 };
