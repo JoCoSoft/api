dfcfb92e14485037ef1666ddd4e91162beb0f927
Eddie Hedges <eddiehedges@gmail.com>
2019-04-19 11:16:10 -0500

    Add CORS support via ENABLE_CORS env var (#12)

--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { JobController, VentController, UserController } from "./controllers";
-
+import cors from "cors";
 // Uncomment to use passport at the router level as opposed to a single route
 // import passport from "passport";
 // require("./passport");
@@ -10,6 +10,39 @@ dotenv.config();
 
 // Create and configure a new express application
 const app: express.Application = express();
+
+const envEnableCors = process.env.ENABLE_CORS;
+if (
+  typeof envEnableCors === "string" &&
+  envEnableCors.trim().toLowerCase() === "true"
+) {
+  // Docs: https://ionicframework.com/docs/faq/cors#local-development-in-the-browser
+  const allowedOrigins = [
+    "capacitor://localhost",
+    "ionic://localhost",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8100"
+  ];
+
+  // Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+  const corsOptions = {
+    origin: (origin: any, callback: any) => {
+      if (allowedOrigins.indexOf(origin) > -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Origin not allowed by CORS"));
+      }
+    }
+  };
+
+  // Enable preflight requests for all routes
+  app.options("*", cors(corsOptions));
+
+  // Enable CORS as router middleware
+  app.use(cors(corsOptions));
+}
+
 app.use(express.json());
 app.use("/api/v1/jobs", JobController);
 app.use("/api/v1/vents", VentController);
